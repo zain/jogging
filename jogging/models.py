92f883a38614eb701a9904dc85e3356c83931e59
Zain Memon <zain@inzain.net>
2009-10-27 17:49:26 -0700

    Using the name of the logger as the source for the DatabaseHandler

--- jogging/models.py
+++ jogging/models.py
@@ -5,4 +5,4 @@ class Log(models.Model):
     datetime = models.DateTimeField(default=datetime.datetime.now)
     level = models.CharField(max_length=128)
     msg = models.TextField()
-    source = models.CharField(max_length=128)
+    source = models.CharField(max_length=128, blank=True)
