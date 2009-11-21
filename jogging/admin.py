0cd3e20a9ab8634afc75259c6c8fea6e63ebdb2f
Chip Tol <wiremine@gmail.com>
2009-11-21 11:04:24 -0500

    Enhanced LogAdmin with date_hierarchy and added 'msg' to search_fields.

--- jogging/admin.py
+++ jogging/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from jogging.models import Log
 
 class LogAdmin(admin.ModelAdmin):
+    date_hierarchy = 'datetime' 
     model = Log
     list_display = ['datetime', 'level', 'source', 'msg']
-    search_fields = ['source']
+    search_fields = ['source', 'msg']
     list_filter = ['level', 'source']
 
 admin.site.register(Log, LogAdmin)
